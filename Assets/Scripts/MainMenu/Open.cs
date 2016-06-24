f2fe5e07ab39aed6b2d23eb415d5b5b09b3be155
fromanint <fromaninteractive@gmail.com>
2016-06-24 18:26:16 -0500

    Firs Commit

---
+++ Assets/Scripts/MainMenu/Open.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Open : MonoBehaviour {
+
+    [SerializeField]
+    string objective;
+
+    public void OpenScene() {
+        SceneManager.LoadScene(objective);
+    }
+
+    public void OpenUrl() {
+        Application.OpenURL(objective);
+    }
+
+    public void ShareFB() {
+    }
+
+    public void OpenAd() {
+    }
+}
