a22c2a3b7342b82f15c317d6639c22703d85dac4
fromanint <RomanInteractive@FROMAN.local>
2016-07-13 15:11:32 -0500

    Start Guessing game

---
+++ Assets/Project/Scripts/Guess/NoteGuess.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteGuess : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerExit2D(Collider2D other){
+		Destroy (gameObject);
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Trigger Stay");
+		}
+	}
+}
