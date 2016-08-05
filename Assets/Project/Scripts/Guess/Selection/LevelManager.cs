0390e26150f66bbf6326baec137b5d9a1b3a2d73
fromanint <fromaninteractive@gmail.com>
2016-08-05 18:52:47 -0500

    fixing levels menu

--- Assets/Project/Scripts/Guess/Selection/LevelManager.cs
+++ Assets/Project/Scripts/Guess/Selection/LevelManager.cs
@@ -4,39 +4,77 @@ using System.Collections;
 
 
 public class LevelManager : MonoBehaviour {
-    [SerializeField]
-	LevelInfo[] levels;
+   
     [SerializeField]
     LevelButton levelsButton;
 	[SerializeField] int maxLevels;
+    [SerializeField] ConfirmPanel confirmPanel;
 
-
+    SpawnLevel sl;
+   
+    int levels;
 	// Use this for initialization
 	void Awake () {
-		ConfirmPanel CP= FindObjectOfType<ConfirmPanel> ();
-		SpawnLevel sl = FindObjectOfType<SpawnLevel> ();
+	   // CP= FindObjectOfType<ConfirmPanel> ();
+		sl = FindObjectOfType<SpawnLevel> ();
+        levels = 0;
+        GenerateLevels(maxLevels);
+	}
 
-		for (int i = 0; i < sl.GetLevels().Length; i++)
+    public void Next()
+    {
+        if (levels < sl.GetLevels().Length)
         {
+            ClearLevels();
+            GenerateLevels(levels + maxLevels);
+        }
+        else
+        {
+            Debug.Log("no more levels");
+        }
+    }
+
+    public void Previous()
+    {
+
+        
+    }
+
+    void ClearLevels()
+    {
+        foreach (Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+    }
+
+    void GenerateLevels(int maxlevel)
+    {
+        //CP = FindObjectOfType<ConfirmPanel>();
+        int lastlLevel = levels;
+        for (levels = lastlLevel; levels < sl.GetLevels().Length; levels++)
+        {
+            if (levels >= maxlevel)
+            { break; }
             LevelButton newButton = Instantiate(levelsButton) as LevelButton;
             newButton.transform.SetParent(transform, false);
 
-			LevelInfo li = sl.Spawn (i);
+            LevelInfo li = sl.Spawn(levels);
 
 
-			newButton.CopyLevelInfo (li);
-			newButton.DisplayInfo ();
-			Destroy (li.gameObject);
+            newButton.CopyLevelInfo(li);
+            newButton.DisplayInfo();
+            Destroy(li.gameObject);
 
-			if (!CP) {
-				Debug.Log ("confirm panel not found");
-			}
-			else {
-				newButton.confirmPanel = CP;
-			}
-        
+            if (!confirmPanel)
+            {
+                Debug.Log("confirm panel not found");
+            }
+            else {
+                newButton.confirmPanel = confirmPanel;
+            }
         }
-	}
-	
+    }
+
 
 }
